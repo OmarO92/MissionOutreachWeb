bb0b4170204d108c039b7f2de741c8569292d104
cdc7 <cchavez44@csub.edu>
2017-10-15 00:28:51 -0700

    Chnages to Home table and style.css
    A bootstrap table template was used to constrict the table that appears on our homepage which has the ability to sort the entries that are listed, search the whole table, and chosse how many entries are seen at one time. Also on the table an additional field was added so that the needs that each client has is shown as well.

--- home.php
+++ home.php
@@ -7,13 +7,16 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
 
 <head>
-  <title>clientloyees</title>
+  <title>MissionOutreach Home</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link rel="stylesheet" href="ss.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"/>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
   <link href="style.css" rel = "stylesheet" text="text/css"/>
 
 </head>
@@ -23,6 +26,11 @@ jQuery(document).ready(function($) {
     $(".clickable-row").click(function() {
         window.location = $(this).data("href");
     });
+});
+$(document).ready(function() {
+    $('#clientTable').DataTable();
+    $('select').addClass('mdb-select');
+    $('.mdb-select').material_select();
 });
 </script>
 
@@ -67,7 +75,7 @@ if(isset($_SESSION['active'])) {
   $link = mysqli_connect('localhost', 'guest', 'Scheema342', 'hackathon');
   
   
-  $client_sql = 'SELECT * FROM client';
+  $client_sql = 'SELECT c.fname, c.lname, concat(CASE n.food WHEN 1 THEN \'food \' ELSE \'\' END, CASE n.shelter WHEN 1 THEN \'shelter \' ELSE \'\' END, CASE n.medical WHEN 1 THEN \'medical\' ELSE \'\' END) AS Needs FROM `client` c, `need` n WHERE c.id = n.forclient';
   $client_parse = mysqli_query($link, $client_sql);
   //$client = oci_fetch_array($client_parse,OCI_BOTH);
   
@@ -81,34 +89,29 @@ if(isset($_SESSION['active'])) {
   echo'
   <center>
   <div class="well col-lg-10 col-lg-offset-1">
-        <div class="panel panel-primary">
-          <div class="panel-heading">clients</div>
-          <div class="panel-body">
-            <div class="table-responsive">
-              <table id="myTable" class="table table-striped" >
-                <thead>
-                  <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                  </tr>
-                </thead>
-                <tbody>';
-                  while ($client = mysqli_fetch_assoc($client_parse)) {
-                      $tclientC++;
-                      echo '<tr>
-                          <td class = "clickable-row" data-href="https://google.com/">'. $client['fname'] .'</td>
-                          <td>'. $client['lname'] .'</td>
-                          </tr>
-                          ';
-                  }
-                  echo'
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-      </center>
+    <table id="clientTable" class="table table-striped table-bordered table-responsive" cellspacing="0" width="100%" >
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Need(s)</th>
+        </tr>
+      </thead>
+      <tbody>';
+        while ($client = mysqli_fetch_assoc($client_parse)) {
+            $tclientC++;
+            echo '<tr>
+                <td class = "clickable-row" data-href="https://google.com/">'. $client['fname'] .'</td>
+                <td>'. $client['lname'] .'</td>
+                <td>'. $client['Needs'] .'</td>
+                </tr>
+                ';
+        }
+        echo'
+      </tbody>
+    </table>
+  </div>
+  </center>
   
   </body>
   </html>';
